dade7ab77276c8bf7ebc46a72e15f2541e6fe88b
Cauã Ramos <cauamedinax@gmail.com>
2025-12-19 09:50:53 -0300

    Update index.ts

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,2 +1,14 @@
-console.log("Hello word")
-console.log("Python eo melhor")
+import fastify from "fastify";
+
+const app = fastify()
+
+const PORT: number = 3000
+const HOST: string = "0.0.0.0"
+
+app.get('/', async (req, res) => {
+    return {message: "ok"}
+})
+
+app.listen({port: PORT, host: HOST}).then(() => {
+    console.log(`Funcionando na porta ${PORT}`)
+}) 
