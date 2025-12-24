7940987fffb5fc0f6cd5d696bc4e72b4974a533d
caua1503 <cauamedinax@gmail.com>
2025-12-24 08:53:54 -0300

    add: Schemas and Interfaces Zod

---
+++ app/src/types/schemas/common.schemas.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const SymbolSchema = z.object({
+    symbol: z.string().min(1)
+})
+
+
+export const IdSchema = z.object({
+    id: z.coerce.number().int().positive()
+});
