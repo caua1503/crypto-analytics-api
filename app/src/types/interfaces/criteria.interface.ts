11aef75f925688a5bb46e175d707fb86978562ac
caua1503 <cauamedinax@gmail.com>
2025-12-28 12:16:50 -0300

    Create: Cria todos os tipos/schemas para compatibilidade com o Banco

---
+++ app/src/types/interfaces/criteria.interface.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+import { CriterionCategory } from "../common.js";
+import { zDecimal, zDecimaltoString } from "../common.js";
+
+export const Criterion = z.object({
+	id: z.number(),
+	code: z.string(),
+	name: z.string(),
+	description: z.string().nullable(),
+	category: CriterionCategory,
+
+	// weights: CriterionWeight[]
+
+	// createdAt: z.coerce.date(),
+});
+
+export const CriterionCreate = Criterion.omit({ id: true });
+
+export const CriterionWeight = z.object({
+	id: z.number(),
+	engineVersionId: z.number(),
+	criterionId: z.number(),
+
+	importanceWeight: zDecimaltoString, //decimal
+	scoreMin: z.number(),
+	scoreMax: z.number(),
+});
+
+export const CriterionWeightCreate = CriterionWeight.omit({ id: true }).extend(
+	{ importanceWeight: zDecimal }, //decimal
+);
+
+export type CriterionType = z.infer<typeof Criterion>;
+export type CriterionCreateType = z.infer<typeof CriterionCreate>;
+
+export type CriterionWeightType = z.infer<typeof CriterionWeight>;
+export type CriterionWeightCreateType = z.infer<typeof CriterionWeightCreate>;
--- app/src/types/interfaces/market.interface.ts
+++ app/src/types/interfaces/market.interface.ts
@@ -1,35 +1,25 @@
 import { Asset } from "./asset.interface.js";
 import { z } from "zod";
 
-export const MarketSnapshot  = z.object({
-    id: z.number(),
-    assetId: z.number(),
-    // asset: Asset;
+export const MarketSnapshot = z.object({
+	id: z.number(),
+	assetId: z.number(),
+	// asset: Asset,
 
-    priceUsd: z.number(),
-    volumeUsd24Hr: z.number(),
-    marketCapUsd: z.number(),
+	priceUsd: z.number(),
+	volumeUsd24Hr: z.number(),
+	marketCapUsd: z.number(),
 
-    btcDominance: z.number(),
-    changePercent24Hr: z.number(),
+	btcDominance: z.number(),
+	changePercent24Hr: z.number(),
 
-    createdAt: z.date(),
+	createdAt: z.coerce.date(),
 });
 
-
-
-export const MarketSnapshotCreate = z.object({
-    assetId: z.number().int().positive(),
-    // asset: Asset;
-
-    priceUsd: z.number().nonnegative(),
-    volumeUsd24Hr: z.number().nonnegative(),
-    marketCapUsd: z.number().nonnegative(),
-
-    btcDominance: z.number().nonnegative(),
-    changePercent24Hr: z.number(),
+export const MarketSnapshotCreate = MarketSnapshot.omit({
+	id: true,
+	createdAt: true,	
 });
-
 
 export type MarketSnapshotType = z.infer<typeof MarketSnapshot>;
 export type MarketSnapshotCreateType = z.infer<typeof MarketSnapshotCreate>;
