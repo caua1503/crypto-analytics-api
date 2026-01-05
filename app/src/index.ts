35f22f877261d1807ec3c5d76f754b986c66774c
Cauã Ramos <cauamedinax@gmail.com>
2026-01-05 11:38:02 -0300

    Update index.ts

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,9 +1,22 @@
 import fastify, { FastifyInstance } from "fastify";
+import sensible from '@fastify/sensible'
+import { validatorCompiler, serializerCompiler, ZodTypeProvider } from 'fastify-type-provider-zod';
+import { env } from "./config/env.js";
+import { CoinGeckoService } from "./core/integrations.js";
+console.log(env, "\n")
 
 const app: FastifyInstance = fastify()
 
-const PORT: number = 3000
-const HOST: string = "0.0.0.0"
+const PORT: number = env.PORT
+const HOST: string = env.HOST
+
+// const coinGeckoService = new CoinGeckoService()
+
+// coinGeckoService.fetchMarketData("solana").then((data) => {
+//     console.log("Dados do Bitcoin buscados com sucesso")
+// }).catch((error) => {
+//     console.error("Erro ao buscar dados do Bitcoin:", error)
+// })
 
 app.get('/', async (req, res) => {
     return {message: "ok"}
