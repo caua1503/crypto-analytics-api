19ed1f849d353af6e1f51180fef1f2cf8a9265df
caua1503 <cauamedinax@gmail.com>
2026-01-19 09:53:47 -0300

    Update asset.interface.ts

--- app/src/types/interfaces/asset.interface.ts
+++ app/src/types/interfaces/asset.interface.ts
@@ -2,9 +2,11 @@ import { z } from "zod";
 
 export const Asset = z.object({
     id: z.number(),
-    symbol: z.string().min(1),
-    name: z.string().min(1),
-
+    symbol: z.string().min(1).toUpperCase(),
+    name: z
+        .string()
+        .min(1)
+        .transform((str) => str.charAt(0).toUpperCase() + str.slice(1)),
     // snapshots MarketSnapshot[]
     // analyses  Analysis[]
 
