b3c57884a7c8eccfd03c8eaec6b20fe2292e48ff
Cauã Ramos <cauamedinax@gmail.com>
2026-02-09 11:12:53 -0300

    Update prisma.config.ts

--- apps/shared/prisma.config.ts
+++ apps/shared/prisma.config.ts
@@ -4,11 +4,11 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  datasource: {
-    url: process.env["MIGRATE_DB"],
-  },
+    schema: "prisma/schema.prisma",
+    migrations: {
+        path: "prisma/migrations",
+    },
+    datasource: {
+        url: process.env["MIGRATE_DB"],
+    },
 });
