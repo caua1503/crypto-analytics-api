06f633ea40b47a9e59347b337a99ba8d1b6e0f64
Cauã Ramos <cauamedinax@gmail.com>
2026-02-11 09:12:17 -0300

    Update daily.scheduler.ts

--- apps/scheduler/src/schedulers/daily.scheduler.ts
+++ apps/scheduler/src/schedulers/daily.scheduler.ts
@@ -3,8 +3,8 @@ import { dispatchQueue } from "../queues/processing.queue";
 export async function registerDailyScheduler() {
   await dispatchQueue.upsertJobScheduler(
     "daily-process",
-    // { every: 10_000 },
-    { pattern: "0 22 * * *" },
+    { every: 60_000 },
+    // { pattern: "0 22 * * *" },
     {
       name: "dispatch-heavy",
       data: {}
