2f1ff29e40040a9c37373abbe21139a62ab30987
caua1503 <cauamedinax@gmail.com>
2026-02-13 10:23:45 -0300

    Create security.ts

---
+++ apps/api/src/core/security.ts
@@ -0,0 +1,10 @@
+import { jwt, jwtType } from "../types/common.js";
+
+function getPasswordHash() {}
+
+function verifyPassword() {}
+
+function createAcessToken() {}
+function createRefreshToken() {}
+
+async function hasAcess() {}
