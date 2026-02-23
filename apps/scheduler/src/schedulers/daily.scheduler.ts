46189c756f90d3d8a3f224da371fe272216e911c
Cauã Ramos <cauamedinax@gmail.com>
2026-02-23 13:24:19 -0300

    Update daily.scheduler.ts

--- apps/scheduler/src/schedulers/daily.scheduler.ts
+++ apps/scheduler/src/schedulers/daily.scheduler.ts
@@ -6,8 +6,9 @@ export async function registerDailyScheduler() {
     {
       // every: 10_000,
       //sempre alterar a data para o dia seguinte ao deploy
-      every: 21_600_000,
-      startDate: new Date('2026-02-18T18:00:00Z'), // start on February 18, 2026
+      // every: 21_600_000,
+      // startDate: new Date('2026-02-18T18:00:00Z'), // start on February 18, 2026
+      pattern: "0 */6 * * *", // a cada 6h, minuto 0
     },
     // { pattern: "0 22 * * *" },
     {
