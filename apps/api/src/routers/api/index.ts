4d6395b4d8c20f70ca9be9e59048ef0723876d6c
Cauã Ramos <cauamedinax@gmail.com>
2026-05-18 09:36:39 -0300

    Update index.ts

--- apps/api/src/routers/api/index.ts
+++ apps/api/src/routers/api/index.ts
@@ -3,7 +3,4 @@ import { registerRoutes as registerV1Routes } from "./v1/index.js";
 
 export async function registerApiRoutes(app: FastifyInstance) {
 	app.register(registerV1Routes, { prefix: "/v1" });
-	// Futuras versões podem ser adicionadas aqui:
-	// app.register(registerV2Routes, { prefix: '/v2' });
-	// app.register(registerV3Routes, { prefix: '/v3' });
 }
